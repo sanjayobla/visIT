c466cd783e46ee72b85831dc961e8dc906e3e7f6
Pranav Ram <pranav.ram7@gmail.com>
2015-04-15 16:25:14 -0400

    fix pan on main content

--- src/plugins/js/zynga-scroller/angular-zynga-scroller.js
+++ src/plugins/js/zynga-scroller/angular-zynga-scroller.js
@@ -70,7 +70,7 @@ angular.module('zyngaScroller', [])
 								console.log(angular.element(e.target));
 								if(angular.element(e.target).hasClass('panel-heading') 
 									|| angular.element(e.target).hasClass('entity')
-									|| angular.element(e.target)[1].nodeName === 'circle'
+									|| angular.element(e.target)[0].nodeName === 'circle'
 									|| angular.element(e.target).hasClass('label')) return;
 
 								scroller.doTouchStart([{
